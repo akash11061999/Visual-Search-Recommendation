da5adccb0d86975bab28309c8c120f73204b9026
Akash Goyal <goyal3593@gmail.com>
2021-03-26 18:43:21 +0530

    Fixed issue in triplet generator script

--- image-similarity/triplet_generator.py
+++ image-similarity/triplet_generator.py
@@ -6,8 +6,14 @@ file1 = open("/home/akash/Downloads/Major_Project/triplet_q.txt", "w")
 file2 = open("/home/akash/Downloads/Major_Project/triplet_p.txt", "w")
 file3 = open("/home/akash/Downloads/Major_Project/triplet_n.txt", "w")
 
-for x in file:
-    l = list(x.split(","))
+dataset = file.readlines()
+print(len(dataset))
+count = 0
+
+for data in dataset:
+    count+=1
+    l = list(data.split(","))
+    print(count, len(l))
     if (os.path.exists(l[0]) and os.path.exists(l[1]) and os.path.exists(l[2])):
         file1.write(l[0]+"\n")
         file2.write(l[1]+"\n")
